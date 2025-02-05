09e93650f661a4524c34959135a1ecc8a6dc7305
Omar Roano Aguilar <omarra77@gmail.com>
2025-02-04 20:19:28 -0600

    Tema: Tipos de datos

---
+++ tipos-de-dato.js
@@ -0,0 +1,42 @@
+// Este es un comentario en JavaScript
+
+//console.log es una funcion que se utiliza para imprimir en consola
+console.log("Hola mundo");
+
+//Declaracion de variables
+var nombre = "Omar"; //String
+var edad = 29; //Number
+var sueldo = 1.2345 //Number
+var casado = false //Boolean
+var hijos = null //Null
+var dato //Undefined
+
+//typeof se utiliza para saber el tipo de dato de una variable
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casado));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+
+
+//NaN (Not a Number) es un valor especial que indica que una operacion matematica no es un numero
+
+// Si ponemos typeof de null dira que es objeto pero en realidad
+//es la ausencia intencionada de valor. 
+
+/*Comentario en bloque */
+
+
+//Ejercicios de CAMPUS 
+
+console.log(typeof(42));
+console.log(typeof('veinticinco'));
+console.log(typeof("veinticinco"));
+console.log(typeof(true));
+console.log(typeof(ABC));
+console.log(typeof(-123));
+console.log(typeof(false));
+console.log(typeof(FALSE));
+console.log(typeof(" "));
+console.log(typeof(_));
