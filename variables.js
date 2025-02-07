4aa8bb93a89564db9d2666bf2c07ac44ecdef89c
Omar Roano Aguilar <omarra77@gmail.com>
2025-02-06 18:59:42 -0600

    Clase 2: Variables

---
+++ variables.js
@@ -0,0 +1,24 @@
+//var es una forma antigua de declarar variables, let es la forma moderna
+var nombre = "Ana";
+
+var nombre = "Fernanda";
+
+var nombre = "Victor";
+
+console.log(nombre);
+
+//let no permite redeclarar variables, loq eu significa que no puedes declarar la misma variable dos veces
+
+let apellido1 = "Gutierrez";
+
+let apellido2 = "Lopez";
+
+//let SI permite el valor, reasignacion de valores
+
+apellido1 = "Perez";
+console.log(apellido1);
+
+//const es una constante, no se puede reasignar el valor 
+const PI = 3.1416;
+
+//PI = 4; //ESTO NO SE PUEDE HACER
