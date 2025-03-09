65eed10898c829e05f00a0c11bd27117f3432824
Omar Roano Aguilar <omarra77@gmail.com>
2025-03-09 16:47:29 -0600

    Se corrigen errores en práctica de caja de comentarios

--- practica_DOM_JavaScript/script.js
+++ practica_DOM_JavaScript/script.js
@@ -2,27 +2,33 @@ document.addEventListener('DOMContentLoaded', () => {
     
     let btnAgregar = document.getElementById("btnAgregar");
 
-    btnAgregar.addEventListener('click', (e) =>{
-        
+    btnAgregar.addEventListener('click', () => {
         let comentario = document.getElementById("comentarioEntrada").value;
         let cajaComentario = document.getElementById("cajaComentario");
         let listaComentarios = document.createElement("div");
+        let textoComentario = document.createElement("p");
         let fecha = new Date();
         let timestamp = fecha.toLocaleString();
+        let timestampSpan = document.createElement("span");
         let btnBorrar = document.createElement("button");
 
         if (comentario.trim() === "") return;
-        
+
         listaComentarios.classList.add("comentario");
-            
-        listaComentarios.textContent = comentario +"__________________" +timestamp; 
+        textoComentario.textContent = comentario;
+        timestampSpan.textContent = " " + timestamp;
+        timestampSpan.style.fontSize = "12px";
+        timestampSpan.style.color = "gray";
+
         btnBorrar.innerText = "Eliminar";
         btnBorrar.classList.add("btnBorrar");
 
-        btnBorrar.onclick = function() {
+        btnBorrar.onclick = function () {
             cajaComentario.removeChild(listaComentarios);
         };
-        
+
+        listaComentarios.appendChild(textoComentario);
+        listaComentarios.appendChild(timestampSpan);
         listaComentarios.appendChild(btnBorrar);
         cajaComentario.appendChild(listaComentarios);
 
